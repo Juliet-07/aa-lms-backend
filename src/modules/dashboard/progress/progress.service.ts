e3c4490d1c02308dbf8589c5c3a507e00dce47a6
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2026-02-06 05:58:22 +0200

    module 1 segment increased to 21

--- src/modules/dashboard/progress/progress.service.ts
+++ src/modules/dashboard/progress/progress.service.ts
@@ -22,7 +22,7 @@ export class ProgressService {
       title: 'Understanding the Foundations of PPR and CLM',
       description:
         'Introducing global and African PPR systems and CLM as a mechanism for accountability, justice, and participation',
-      parts: 17,
+      parts: 21,
     },
     {
       moduleId: 2,
